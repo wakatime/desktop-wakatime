56bf13052b0e3a30449b4a81d77bbf694ddf421f
Furukawa Atsushi <atsushifx@gmail.com>
2025-06-05 08:39:59 +0900

    fix(config): respect WAKATIME_HOME env for config file location (#105)
    Use getWakatimeHome to resolve config path based on WAKATIME_HOME env var,
    falling back to user home dir if unset. This ensures wakatime.cfg is placed
    correctly. Updated references to use the new utility.
    Closes #105

--- electron/helpers/config-file.ts
+++ electron/helpers/config-file.ts
@@ -1,15 +1,14 @@
 import path from "node:path";
-import { app } from "electron";
 
-import { getResourcesFolderPath } from "../utils";
+import { getResourcesFolderPath, getWakatimeHome } from "../utils";
 import { ConfigFileReader } from "./config-file-reader";
 
 export abstract class ConfigFile {
   static getConfigFilePath(internalConfig = false) {
+    const userHome = getWakatimeHome();
     if (internalConfig) {
       return path.join(getResourcesFolderPath(), "wakatime-internal.cfg");
     } else {
-      const userHome = app.getPath("home");
       return path.join(userHome, ".wakatime.cfg");
     }
   }
